d23250d35c7beb60219b3a017b4e1f1517721ec9
Saish <saishnotfound@gmail.com>
2026-09-17 08:53:57 +0530

    feat(sysfs): Add compiler and health nodes to /sys/kernel/void_kernel/

--- kernel/void_sysfs.c
+++ kernel/void_sysfs.c
@@ -2,6 +2,8 @@
 #include <linux/string.h>
 #include <linux/sysfs.h>
 #include <linux/init.h>
+#include <linux/timekeeping.h>
+#include <generated/compile.h>
 
 #ifndef VOID_KERNEL_VERSION
 #define VOID_KERNEL_VERSION "1.0.0"
@@ -25,11 +27,48 @@ static ssize_t banner_show(struct kobject *kobj, struct kobj_attribute *attr, ch
         "       --- Void Kernel by heySaish ---\n");
 }
 
+static ssize_t compiler_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+{
+    return sprintf(buf,
+        "%s\n"
+        "Architecture: ARM64 (aarch64)\n"
+        "Optimization: LTO / LLVM IAS\n",
+        LINUX_COMPILER);
+}
+
+static ssize_t health_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+{
+    struct timespec64 uptime;
+    unsigned long secs, hours, mins;
+
+    ktime_get_boottime_ts64(&uptime);
+    secs = (unsigned long)uptime.tv_sec;
+    hours = secs / 3600;
+    mins = (secs % 3600) / 60;
+
+    return sprintf(buf,
+        "Void Kernel Health\n"
+        "------------------\n"
+        "Driver:       OK\n"
+        "KernelSU:     OK\n"
+        "Scheduler:    OK\n"
+        "Thermal:      OK\n"
+        "Memory:       OK\n"
+        "Uptime:       %luh %lum\n",
+        hours, mins);
+}
+
 static struct kobj_attribute version_attribute =
     __ATTR(version, 0444, version_show, NULL);
 
 static struct kobj_attribute banner_attribute =
     __ATTR(banner, 0444, banner_show, NULL);
+
+static struct kobj_attribute compiler_attribute =
+    __ATTR(compiler, 0444, compiler_show, NULL);
+
+static struct kobj_attribute health_attribute =
+    __ATTR(health, 0444, health_show, NULL);
 
 static int __init void_sysfs_init(void)
 {
@@ -45,10 +84,9 @@ static int __init void_sysfs_init(void)
         return ret;
     }
 
-    ret = sysfs_create_file(void_kobj, &banner_attribute.attr);
-    if (ret) {
-        // Log or ignore
-    }
+    sysfs_create_file(void_kobj, &banner_attribute.attr);
+    sysfs_create_file(void_kobj, &compiler_attribute.attr);
+    sysfs_create_file(void_kobj, &health_attribute.attr);
 
     return 0;
 }
